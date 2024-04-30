270c58fc837ed12b1bb2f52961ce68a52e0f085f
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-04-30 22:09:03 +0300

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -19,13 +19,13 @@ let img = await q.download?.()
 stiker = await mp4ToWebp(img, { pack: packname || global.packname, author: author || global.author })
 } else if (args[0] && isUrl(args[0])) {
 stiker = await createSticker(false, args[0], '', author, 20)
-} else throw `*رجاء قم بالاشارة للصورة التي تريد أن تحولها لملصق و اكتب الامر *.sticker* ${usedPrefix + command}*`
+} else throw `*رجاء قم بالاشارة للصورة التي تريد أن تحولها لملصق و اكتب الامر *.ملصق* ${usedPrefix + command}*`
 } catch {
-stiker = 'رجاء قم بالاشارة للصورة التي تريد أن تحولها لملصق و اكتب الامر \n *.sticker*'	
+stiker = 'رجاء قم بالاشارة للصورة التي تريد أن تحولها لملصق و اكتب الامر \n *.ملصق*'	
 } finally {
 m.reply(stiker)}}
-handler.help = ['sticker']
-handler.tags = ['sticker']
+handler.help = ['sticker','ملصق']
+handler.tags = ['sticker','ملصق']
 handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
 export default handler
 const isUrl = (text) => text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
