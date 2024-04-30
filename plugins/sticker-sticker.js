7ecac685ca543dab9bff033b777b79e878233381
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-04-30 22:11:25 +0300

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -24,7 +24,7 @@ stiker = await createSticker(false, args[0], '', author, 20)
 stiker = 'رجاء قم بالاشارة للصورة التي تريد أن تحولها لملصق و اكتب الامر \n *.ملصق*'	
 } finally {
 m.reply(stiker)}}
-handler.help = ['sticker','ملصق']
+handler.help = ['ملصق']
 handler.tags = ['sticker','ملصق']
 handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
 export default handler
