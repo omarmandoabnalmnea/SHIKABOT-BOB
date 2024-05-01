e6442d34b63dadac1d39c333500eeae5ac54d8ba
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-01 10:47:08 +0300

    Update Dragon-ball-z.js

--- plugins/Dragon-ball-z.js
+++ plugins/Dragon-ball-z.js
@@ -31,7 +31,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
   }
 }
 
-handler.help = ['dragon-ball-z']
+handler.help = ['dragon-ball-z','دراغون-بول']
 handler.tags = ['anime']
 handler.command = /^dragon-ball-z$/i
 export default handler
