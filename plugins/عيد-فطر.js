f1e493efb031103b85c148921ca36e567f2f49ba
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-03 05:18:12 +0300

    Update and rename aidfitr.js to عيد-فطر.js

--- plugins/عيد-فطر.js
+++ plugins/عيد-فطر.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 const handler = async (m, { conn, args, text, command, usedPrefix, isCreator, isPrems }) => {
   let response = args.join(' ').split('|');
-  if (!args[0]) return conn.reply(m.chat, `ارسل تهنئة عيد الفطر لصديقك مثال :\n\n*${usedPrefix}${command}  noureddine ♥*`, m);
+  if (!args[0]) return conn.reply(m.chat, `عيد عل صتحبك ياض🦦 مثال :\n\n*${usedPrefix}${command}  كل سنه وانت طيب يابرو بحبك والله حبيبي حبيبي والله🫂🦦*`, m);
   conn.sendMessage(m.chat, {
     react: {
       text: '🕒',
@@ -12,8 +12,8 @@ const handler = async (m, { conn, args, text, command, usedPrefix, isCreator, is
   let res = `https://api.lolhuman.xyz/api/idulfitri?apikey=Gatadios&text=${response[0]}`;
   conn.sendFile(m.chat, res, 'idulfitri.jpg', 'instagram.com/noureddine_ouafy', m, false);
 };
-handler.help = ['aidfitr'];
+handler.help = ['عيد-فطر'];
 handler.tags = ['logo'];
-handler.command = /^(aidfitr)$/i;
+handler.command = /^(عيد-فطر)$/i;
 
 export default handler;
