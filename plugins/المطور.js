5845c598b1acb812a0adb218f12da9814feb73de
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-04 20:55:58 +0300

    Update and rename channel.js to المطور.js

---
+++ plugins/المطور.js
@@ -0,0 +1,11 @@
+let handler = async (m, { conn }) => {
+
+m.reply(`*المطـور ♕𝙎𝙃𝙄𝙆𝘼🐥♕:*\n
+*https://wa.me/+201005603546*
+*تنــور ياحب🦦*`)
+}
+handler.help = ['المطور']
+handler.tags = ['infobot']
+handler.command = /^(المطور)$/i
+
+export default handler;
