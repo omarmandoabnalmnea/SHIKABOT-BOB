f4799365c68d038c624f4c1f0b49d563235ef985
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-05 06:35:19 +0300

    Update and rename join.js to دخول لمجموعه.js

--- plugins/دخول لمجموعه.js
+++ plugins/دخول لمجموعه.js
@@ -2,18 +2,18 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 
 let handler = async (m, { conn, text, isOwner }) => {
     let [_, code, expired] = text.match(linkRegex) || []
-    if (!code) throw 'الرابط غير صالح'
+    if (!code) throw 'الرابط مش شغال ياحب🦦'
     let res = await conn.groupAcceptInvite(code)
     expired = Math.floor(Math.min(999, Math.max(1, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 3)))
-    m.reply(`تم الانضمام للمجموعة بنجاح ${res}${expired ? ` خلال ${expired} يوم` : ''}\n\nتابع صاحب البوت في حسابه\ninstagram.com/noureddine_ouafy`)
+    m.reply(`اشطا دخلت🦦 ${res}${expired ? ` خلال ${expired} يوم` : ''}\n\nتابع صاحب البوت في حسابه\n*『 https://instagram.com/shika17sm... 』*`)
     let chats = global.db.data.chats[res]
     if (!chats) chats = global.db.data.chats[res] = {}
     if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
 }
-handler.help = ['join']
+handler.help = ['دخول-لمجموعه']
 handler.tags = ['owner']
 
-handler.command = /^join$/i
+handler.command = /^دخول-لمجموعه$/i
 handler.rowner = true
 
 export default handler
