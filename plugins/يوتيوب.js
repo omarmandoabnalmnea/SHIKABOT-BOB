a05463fba19be0190f4086da669ebef7d80ec5b6
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-06 13:48:48 +0300

    Create يوتيوب.js

---
+++ plugins/يوتيوب.js
@@ -0,0 +1,28 @@
+import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
+import fetch from 'node-fetch'
+let handler = async (m, { conn, args }) => {
+if (!args[0]) throw '*هذا الامر خاص بالتحميل من اليوتوب* مثال : \n *.فيديو* https://youtu.be/hH1VDYPhgKE?si=sPkYmIdc7HR6Ndh8'
+await m.reply(`*اصبـر ياحته بحمـلك الفيـديو👀* `)
+try {
+let qu = args[1] || '720'
+let q = qu + 'p'
+let v = args[0]
+const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v)).catch(async _ => await youtubedlv3(v))
+const dl_url = await yt.video[q].download()
+const ttl = await yt.title
+const size = await yt.video[q].fileSizeH
+await await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `▢: ${ttl}\n▢ 𝙿: ${size}`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m })
+} catch {
+try {
+let lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytvideo2?apikey=85faf717d0545d14074659ad&url=${args[0]}`)    
+let lolh = await lolhuman.json()
+let n = lolh.result.title || 'error'
+let n2 = lolh.result.link
+let n3 = lolh.result.size
+let n4 = lolh.result.thumbnail
+await conn.sendMessage(m.chat, { video: { url: n2 }, fileName: `${n}.mp4`, mimetype: 'video/mp4', caption: `▢ 𝚃: ${n}\n▢ 𝙿: ${n3}`, thumbnail: await fetch(n4) }, { quoted: m })
+} catch {
+await conn.reply(m.chat, 'فشـل التحميـل يامنحــوس🦦', m)}
+}}
+handler.command = /^فيديو|yt|يوتيوب$/i
+export default handler
