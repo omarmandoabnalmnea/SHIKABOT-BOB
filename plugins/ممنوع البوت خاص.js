3aba343a97d8d72cb03351493274a4dd94bb12c4
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-06 05:18:47 +0300

    Create ممنوع البوت خاص.js

---
+++ plugins/ممنوع البوت خاص.js
@@ -0,0 +1,13 @@
+export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
+  if (m.isBaileys && m.fromMe) return !0;
+  if (m.isGroup) return !1;
+  if (!m.message) return !0;
+  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
+  const chat = global.db.data.chats[m.chat];
+  const bot = global.db.data.settings[this.user.jid] || {};
+  if (bot.antiPrivate && !isOwner && !isROwner) {
+    await m.reply(`*[❗] مرحباً @${m.sender.split`@`[0]},،ممنوع التحدث مغ البوت الخاص عشان كدا ابلع بلوك🦦.*`, false, {mentions: [m.sender]});
+    await this.updateBlockStatus(m.chat, 'block');
+  }
+  return !1;
+}
