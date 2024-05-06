c2162cc3ddac1977be670a68b729e3fceda571f0
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-06 04:41:01 +0300

    Update تطبيق.js

--- plugins/تطبيق.js
+++ plugins/تطبيق.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
   try {    
     const searchA = await search(text);
     const data5 = await download(searchA[0].id);
-    let response = ` تطبيـق🔮• ━ ──⎔\n\n📌 *اسم التطبيق:* ${data5.name}\n🔰 *البـاكج:* ${data5.package}\n⏱️ *تاريـخ التحديث:* ${data5.lastup}\n📥 *حجم التطبيق:* ${data5.size}\n\nياحظـك فاضي تلعــب انا في 3 ثانوي ادعيلـي🦦💔`
+    let response = `* • ╌─━┇تطبيـق🔮*\n\n📌 *اسم التطبيق:* ${data5.name}\n🔰 *البـاكج:* ${data5.package}\n⏱️ *تاريـخ التحديث:* ${data5.lastup}\n📥 *حجم التطبيق:* ${data5.size}\n\n*ياحظـك فاضي تلعــب انا في 3 ثانوي ادعيلـي🦦💔*`
     await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
  if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
       return await conn.sendMessage(m.chat, {text: '*[🗿 ]الملف كبيـر والنـت مش ببـلاش فامش باعـت*.'}, {quoted: m});
