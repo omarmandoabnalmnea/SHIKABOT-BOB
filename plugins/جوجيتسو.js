8e8ae4d59c6d57a72226a2e4c5db2d0e33f61e45
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-06 05:21:02 +0300

    Update and rename jujutsu.js to جوجيتسو.js

--- plugins/جوجيتسو.js
+++ plugins/جوجيتسو.js
@@ -31,7 +31,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
   }
 }
 
-handler.help = ['jujutsu']
+handler.help = ['جوجيتسو']
 handler.tags = ['anime']
-handler.command = /^jujutsu$/i
+handler.command = /^جوجيتسو$/i
 export default handler
