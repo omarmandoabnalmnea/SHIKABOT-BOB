ff07bc3f75435201715b2dc65603f3051c4a276a
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-06 06:38:41 +0300

    Update حب-.js

--- plugins/حب-.js
+++ plugins/حب-.js
@@ -1,41 +1,10 @@
-
-const handler = async (m, { conn, command, text }) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.fun_love
-
-  const lovePercentage = Math.floor(Math.random() * 100);
-  const isHighLove = lovePercentage >= 50;
-  const loveMessages = tradutor.texto1;
-  const notSoHighLoveMessages = tradutor.texto2;
-  const loveDescription = isHighLove ? tradutor.texto3[0] : tradutor.texto3[1];
-  const getRandomMessage = (messages) => messages[Math.floor(Math.random() * messages.length)];
-  const loveMessage = isHighLove ? getRandomMessage(loveMessages) : getRandomMessage(notSoHighLoveMessages);
-  const response =
-    `━━━━━━━⬣ *LOVE* ⬣━━━━━━━\n` +
-    `${tradutor.texto4[0]}, ${text} ${tradutor.texto4[1]} @${m.sender.split('@')[0]} ${loveDescription} ${tradutor.texto4[2]} ${lovePercentage}% ${tradutor.texto4[3]}\n\n` +
-    `*❥ ${loveMessage}*\n` +
-    `━━━━━━━⬣ *LOVE* ⬣━━━━━━━`    
-  
-  async function loading() {
-var hawemod = [
-"《 █▒▒▒▒▒▒▒▒▒▒▒》10%",
-"《 ████▒▒▒▒▒▒▒▒》30%",
-"《 ███████▒▒▒▒▒》50%",
-"《 ██████████▒▒》80%",
-"《 ████████████》100%"
-]
-   let { key } = await conn.sendMessage(m.chat, {text: tradutor.texto5, mentions: conn.parseMention(response)}, {quoted: m})
- for (let i = 0; i < hawemod.length; i++) {
-   await new Promise(resolve => setTimeout(resolve, 1000)); 
-   await conn.sendMessage(m.chat, {text: hawemod[i], edit: key, mentions: conn.parseMention(response)}, {quoted: m}); 
-  }
-  await conn.sendMessage(m.chat, {text: response, edit: key, mentions: conn.parseMention(response)}, {quoted: m});         
- }
-loading()    
-};
-handler.help = ['حب'];
-handler.tags = ['fun'];
-handler.command = /^(حب|amor)$/i;
-export default handler;
+let handler = async (m, { conn, command, text }) => {
+let love = `*✨💜 نسبه الحب 💜✨*
+*نسبة الحب ${text} ليك ✨💜* *${Math.floor(Math.random() * 100)}%* *من 100%*
+*اممم خليكم اصحـاب او اتجوزو المهم حلو عني انا كيميتش🦦 *
+`.trim()
+m.reply(love, null, { mentions: conn.parseMention(love) })}
+handler.help = ['حب']
+handler.tags = ['fun']
+handler.command = /^(حب)$/i
+export default handler
