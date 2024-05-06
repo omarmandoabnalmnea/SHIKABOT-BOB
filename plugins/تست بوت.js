2f9357bb4d9be55ca9b02eb6bab51d86d513d31e
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-06 17:28:11 +0300

    Update تست بوت.js

--- plugins/تست بوت.js
+++ plugins/تست بوت.js
@@ -1,12 +1,12 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-let info = `*البـوت شغـاااال شغال صدعتوني 🐥💔* \n*اكتـب* \n.menu\n *🌚🦦عشان يطلعلك....(يطلعلـك الاوامر)* \n@${m.sender.split('@')[0]} \n`
+let info = `*البـوت شغـاااال شغال صدعتوني 🐥💔* \n*اكتـب* \nالاوامر.\n *عشان يطلعلك....(يطلعلـك الاوامر)🌚🦦* \n@${m.sender.split('@')[0]} \n`
 await conn.reply(m.chat, info, m, { contextInfo: { mentionedJid: [m.sender],forwardingScore: 65,
       isForwarded: true, externalAdReply: { title: author, body: bottime,thumbnail: fs.readFileSync('./thumbnail.jpg') }}})
       await conn.sendMessage(m.chat, {
         audio: {
-            url: "bobizaramadhan.mp3"
+            url: "♕𝙎𝙃𝙄𝙆𝘼🐥ᵇᵒᵗ♕.mp3"
         },
         seconds: 65,
         ptt: true,
