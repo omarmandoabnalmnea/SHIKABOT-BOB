237472bc20eaec67c9230654b4ec0ad56f167240
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-07 19:54:32 +0300

    Update صوره.js

--- plugins/صوره.js
+++ plugins/صوره.js
@@ -5,7 +5,7 @@ if (m.text.includes('gore') || m.text.includes('cp')|| m.text.includes('porno')|
 const res = await googleImage(text)
 let image = await res.getRandom()
 let link = image
-conn.sendFile(m.chat, link, 'error.jpg', `🔎 *صوره ل:**${text}*\n *من* ♕𝙎𝙃𝙄𝙆𝘼🐥ᵇᵒᵗ♕\n🌎 *محرك البحث:* *جوجل*`, m)}
+conn.sendFile(m.chat, link, 'error.jpg', `🔎 *صوره ل🦦:* *${text}*\n *من🥸* ♕𝙎𝙃𝙄𝙆𝘼🐥ᵇᵒᵗ♕\n🌎 *المصـدر📍:* *🔗جوجل*`, m)}
 //let captionn = `🔎 *𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴:* ${text}\n🔗 *𝙻𝙸𝙽𝙺* ${link}\n🌎 *𝙱𝚄𝚂𝙲𝙰𝙳𝙾𝚁:* Google`
 //conn.sendButton(m.chat, captionn, author, link, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `#imagen ${text}`]], m)}
 handler.help = ['gimage <query>', 'imagen <query>']
