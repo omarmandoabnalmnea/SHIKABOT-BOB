4518fa3e29894552338b0ce98dfdfd613c961573
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-08 15:42:35 +0300

    Update and rename plugins/climate.js to الطقس.js

--- الطقس.js
+++ الطقس.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 let handler = async (m, { args }) => {
-if (!args[0]) throw "*اكتب اسم المدينة او البلد الذي تريد ان تعرف مناخه*"
+if (!args[0]) throw "*اكتب اسم المكــان الـي عـايز تعـرف منـاخو🦦*"
 try {
 const response = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${args}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273`)
 const res = await response
@@ -12,11 +12,11 @@ const Minimum_Temperature = res.data.main.temp_min + "°C"
 const Maximum_Temperature = res.data.main.temp_max + "°C"
 const Humidity = res.data.main.humidity + "%"
 const Wind = res.data.wind.speed + "km/h"
-const wea = `「 📍 」PLACE: ${name}\n「 🗺️ 」COUNTRY: ${Country}\n「 🌤️ 」VIEW: ${Weather}\n「 🌡️ 」TEMPERATURE: ${Temperature}\n「 💠 」 MINIMUM TEMPERATURE: ${Minimum_Temperature}\n「 📛 」 MAXIMUM TEMPERATURE: ${Maximum_Temperature}\n「 💦 」HUMIDITY: ${Humidity}\n「 🌬️ 」 WINDSPEED: ${Wind}`
+const wea = `「 🏳 」المكـان: ${name}\n「 🗺️ 」الدولـه: ${Country}\n「 🌤️ 」الجـو: ${Weather}\n「 🌡️ 」درجـة الحـراره: ${Temperature}\n「 ❄ 」 درجــة الحــراره الصغـرى: ${Minimum_Temperature}\n「 🔥 」 درجـة الحـراره العظــمى: ${Maximum_Temperature}\n「 💧 」الرطـوبه: ${Humidity}\n「 🌪 」 سرعـة الريـاح: ${Wind}`
 m.reply(wea)
 } catch {
 return "*ERROR*"}}
-handler.help = ['climate']
+handler.help = ['الطقص']
 handler.tags = ['tools']
-handler.command = /^(climate|weather)$/i
+handler.command = /^(طقس|الطقس)$/i
 export default handler
