b0ae2a6011af35ba13dc583b882eca40d6492321
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-10 16:06:10 +0300

    Update كت.js

--- plugins/كت.js
+++ plugins/كت.js
@@ -5,7 +5,7 @@ handler.before = async function (m) {
     let id = m.chat
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0
     this.tekateki = this.tekateki ? this.tekateki : {}
-    if (!(id in this.tekateki)) return m.reply('لقـد انتهـت هذه اللـعبه اكتـب الامـر مجـدادا للعـب مره اخـرا🦦!')
+    if (!(id in this.tekateki)) return m.reply('اللعـبه خلصـت يسطـا اكتـب الامـر تانـي عشـان تلعـب🦦!')
     if (m.quoted.id == this.tekateki[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.tekateki[id][1]))
         // m.reply(JSON.stringify(json, null, '\t'))
@@ -14,7 +14,7 @@ handler.before = async function (m) {
             m.reply(`*❐┃اجـابـة صـحـيـحـة┃✅ ❯*\n\n*❐↞┇الـجـائـزة💰↞ ${this.tekateki[id][2]} نقطة┇*`)
             clearTimeout(this.tekateki[id][3])
             delete this.tekateki[id]
-        } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`Casi lo logras!`)
+        } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`قربـت تنجـح عـاش🐥!`)
         else m.reply('❐┃اجـابـة خـاطـئـة┃❌ ❯')
     }
     return !0
