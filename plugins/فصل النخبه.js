a28afcfd1d31e5b111a60809a3632d62e3a145a4
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-14 06:29:31 +0300

    Update and rename classroom-ote.js to فصل النخبه.js

--- plugins/فصل النخبه.js
+++ plugins/فصل النخبه.js
@@ -33,5 +33,5 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 
 handler.help = ['classroom-ote']
 handler.tags = ['anime']
-handler.command = /^classroom-ote$/i
+handler.command = /^فصل-النخبه$/i
 export default handler
